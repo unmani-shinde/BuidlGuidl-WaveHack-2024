852c12f85b4dacf0a380c500857e2807a10dffe3
Unmani Shinde <mailto.unmani@gmail.com>
2024-05-10 14:12:24 +0530

    updated input configuration.

--- client/packages/nextjs/app/anon-explorer/page.jsx
+++ client/packages/nextjs/app/anon-explorer/page.jsx
@@ -27,6 +27,22 @@ function AnonymousExplorer() {
   const [modalVisible, setModalVisible] = useState(false);
   const { address: connectedAddress } = useAccount();
   const { writeContractAsync: writeYourContractAsync } = useScaffoldWriteContract("HushZKFactory");
+
+  const [reportData, setReportData] = useState({
+    reportHeading: "",
+    dateOfReport: "",
+    reportCategory: "",
+    reportSummary: "",
+  });
+
+  const handleInputChange = (e) => {
+    const { name, value } = e.target;
+    setReportData({ ...reportData, [name]: value });
+  };
+
+  const convertToJSON = () => {
+    return JSON.stringify(reportData);
+  };
     
     const toggleModal = () => {
     setModalVisible((prevVisible) => !prevVisible);
@@ -40,7 +56,7 @@ function AnonymousExplorer() {
   const handleCreateReport = async()=>{
     
     try {
-      const metadata = "Metadata"
+      const metadata = convertToJSON();
       await writeYourContractAsync({
         functionName: "deployReport",
         args:[metadata]
@@ -132,25 +148,40 @@ function AnonymousExplorer() {
                 <div className="grid gap-4 mb-4 grid-cols-2">
                     <div className="col-span-2">
                         <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Report Heading</label>
-                        <input type="text" name="name" id="name" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type report heading" required="" />
+                        <input type="text"
+              name="reportHeading"
+              id="reportHeading"
+              value={reportData.reportHeading}
+              onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Type report heading" required="" />
                     </div>
                     <div className="col-span-2 sm:col-span-1">
                         <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Date of Report</label>
-                        <input type="number" name="price" id="price" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="DD-MM-YYYY" required="" />
+                        <input type="date"
+              name="dateOfReport"
+              id="dateOfReport"
+              value={reportData.dateOfReport}
+              onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="DD-MM-YYYY" required="" />
                     </div>
                     <div className="col-span-2 sm:col-span-1">
                         <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Report Category</label>
-                        <select id="category" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
+                        <select name="reportCategory"
+              id="reportCategory"
+              value={reportData.reportCategory}
+              onChange={handleInputChange} className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500">
                             <option selected="">Select category</option>
-                            <option value="TV">TV/Monitors</option>
-                            <option value="PC">PC</option>
-                            <option value="GA">Gaming/Console</option>
-                            <option value="PH">Phones</option>
+                            <option value="FM">Financial Misconduct</option>
+                            <option value="SV">Safety Violations</option>
+                            <option value="CM">Corporate malfeasance</option>
+                            <option value="HR">Human Rights Abuses</option>
+                            <option value="GC">Government Corruption</option>
                         </select>
                     </div>
                     <div className="col-span-2">
                         <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Report Summary</label>
-                        <textarea id="description" rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write product description here"></textarea>                    
+                        <textarea name="reportSummary"
+              id="reportSummary"
+              value={reportData.reportSummary}
+              onChange={handleInputChange} rows="4" className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500" placeholder="Write report summary here"></textarea>                    
                     </div>
                 </div>
                 <button onClick={(e) => {e.preventDefault();handleCreateReport()}} type="submit" className="text-white inline-flex items-center bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5">
